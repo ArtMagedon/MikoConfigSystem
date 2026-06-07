a028ac773e09112b7b17c8cc2a10e14746fc2793
ArtMagedon <ap20041026@yandex.ru>
2026-06-07 19:06:08 +0300

    1

--- server.py
+++ server.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route("/api/metrics", methods=['POST'])
 def api_feedback():
+    # 1. Пытаемся прочитать JSON из тела запроса
+    data = request.get_json(silent=True)
+
     print(f"Method: {request.method}, Path: {request.path}")
+
+    # 2. Проверяем, пришел ли JSON, и выводим его
+    if data:
+        print(f"Получены данные (JSON): {json.dumps(data, indent=4, ensure_ascii=False)}")
+    else:
+        print("JSON не получен или пустой запрос")
+
     return "", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True, threaded=False)
+    app.run(host="0.0.0.0", port=8000, debug=True, threaded=False)
