cfd50e84d2837839ca89c8a89ea25e66a950d096
Miko <miko@mikovoyager.aeza.network>
2026-06-19 17:50:04 +0000

    edit sshBrutforce

--- sshBrutforce.sh
+++ sshBrutforce.sh
@@ -1,5 +1,4 @@
 apt update && apt install fail2ban -y
-systemctl status fail2ban
 tee /etc/fail2ban/jail.local > /dev/null <<EOF
 [DEFAULT]
 ignoreip = 127.0.0.1/8 ::1 77.37.196.166 194.33.35.82
@@ -14,3 +13,6 @@ logpath = /var/log/auth.log
 bantime.increment = true
 banaction = iptables-multiport
 EOF
+
+sudo systemctl restart fail2ban
+
